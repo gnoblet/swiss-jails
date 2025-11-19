13ba644caef460ee31b3b04cd7f1e70ebe492fc2
gnoblet <gnoblet@zaclys.net>
2025-11-19 17:14:20 +0100

    feat: Update header content, hero height and styling for Swiss Jails theme

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,18 +4,18 @@
 	}
 </script>
 
-<div class="hero min-h-96">
+<div class="hero min-h-screen">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
-			<h1 class="text-6xl font-bold">Welcome to SvelteKit</h1>
-			<p class="py-6 text-lg">
-				Visit <a href="https://svelte.dev/docs/kit" class="link link-primary"
-					>svelte.dev/docs/kit</a
+			<h1 class="text-7xl font-bold">Swiss Jails Are Jails As Others</h1>
+			<p class="py-6 text-3xl">
+				Visit <a href="https://parlonsprisons.noblogs.org/" class="link link-primary"
+					>Parlons Prisons</a
 				>
-				to read the documentation
+				to read their manifest
 			</p>
 			<button 
-				class="btn btn-ghost btn-outline btn-lg" 
+				class="btn btn-outline btn-lg" 
 				onclick={scrollToContent}
 			>
 				Explore Swiss Jails
