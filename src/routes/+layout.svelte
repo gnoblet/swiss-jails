3862859cca6707ae348d7756ec71fcdb68e842ed
gnoblet <gnoblet@zaclys.net>
2025-11-20 11:24:51 +0100

    feat: Set up GitHub Actions for deployment to GitHub Pages with static adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
+	export const prerender = true;
+
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { base } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href={`${base}${favicon}`} />
 </svelte:head>
 
 <div class="flex justify-center min-h-screen">
