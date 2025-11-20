074ad2aca14c8fa6f9dc4eb367efbd9b8db00e14
gnoblet <gnoblet@zaclys.net>
2025-11-20 14:51:19 +0100

    feat: Add meta tags for SEO and social media sharing in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,23 @@
 </script>
 
 <svelte:head>
+	<title>Les prisons suisses | Swiss Jails</title>
+	<meta name="description" content="Explorez les données sur les prisons suisses. Visitez Parlons Prisons pour en savoir plus." />
+	<meta name="keywords" content="prisons, suisse, swiss, jail, système carcéral, Parlons Prisons" />
+	
+	<!-- Open Graph / Facebook / LinkedIn / Bluesky -->
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Les prisons suisses | Swiss Jails" />
+	<meta property="og:description" content="Explorez les données sur les prisons suisses. Visitez Parlons Prisons pour en savoir plus." />
+	<meta property="og:site_name" content="Swiss Jails" />
+	<meta name="author" content="Guillaume Noblet" />
+	
+	<!-- Twitter / X -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:title" content="Les prisons suisses | Swiss Jails" />
+	<meta property="twitter:description" content="Explorez les données sur les prisons suisses. Visitez Parlons Prisons pour en savoir plus." />
+	<meta name="twitter:creator" content="@gnoblet.bsky.social" />
+	
 	<link rel="icon" href={`${base}/favicon.svg`} />
 </svelte:head>
 
